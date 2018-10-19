0403425d4746211b6cec28aedcbaa5d773c277b2
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2018-10-19 11:59:31 -0700

    updates 1

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    // On click to send an email //
+    $('#gmailLogo').on('click', function (event) {
+        var email = 'awusa888@gmail.com';
+        window.location = 'mailto:' + email + '?';
+    }),
+
+    $()
+
+        console.log("hi");
+    M.AutoInit();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 100,
+    });
+
+});
